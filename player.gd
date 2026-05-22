b3097364f9125154e65a2fa0e5ab91ec0dedf291
Ethan Sahlstrom <sahlstromethan@gmail.com>
2026-05-22 15:27:31 -0700

    Situated Player scene in Main. Adjusted Screen Res, starting player in center of viewport

--- player.gd
+++ player.gd
@@ -2,6 +2,14 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+func _ready():
+	# get center coords of screen
+	var center_position = get_viewport_rect().size / 2
+	
+	# 
+	position = center_position
+
+
 func get_input():
 	look_at(get_global_mouse_position())
 	velocity = transform.x * Input.get_axis("down", "move_forward") * speed
@@ -9,7 +17,11 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-	grow()
+
 
 func grow():
 	scale = scale * 1.5
+
+# currently passing in StartPosition to start in middle of screen
+func start(pos):
+	position = pos
