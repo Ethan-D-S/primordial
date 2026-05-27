c3bd2b903424fc2e086e44a1cdcb08219706086d
Ethan Sahlstrom <sahlstromethan@gmail.com>
2026-05-27 16:54:02 -0700

    wanderers now chase algae

--- wanderer.gd
+++ wanderer.gd
@@ -8,6 +8,7 @@ var target: Node2D = null
 
 func _physics_process(delta: float) -> void:
 	if is_instance_valid(target):
+		print("chasing: ", target.global_position)
 		# normalize position difference so that the vector will be no greater than 1
 		var direction = (target.global_position-global_position).normalized()
 		velocity = lerp(velocity, direction * speed, 10*delta)
@@ -17,19 +18,15 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 
-# triggers enterer's eat()
-func _on_border_body_entered(body: Node2D) -> void:
-	if body.mass > mass:
-		body.eat(mass)
-		queue_free()
+
 
 func find_target() -> Node2D:
 	#get bodies from sight area
-	var bodies = $sight_area.get_overlapping_bodies()
-	
-	for body in bodies:
-		if body.is_in_group("algae"):
-			return body
+	var areas = $sight_area.get_overlapping_areas()
+	#print("areas found: ", areas.size())
+	for area in areas:
+		if area.is_in_group("algae"):
+			return area
 	# no targets
 	return null
 		
@@ -37,3 +34,9 @@ func find_target() -> Node2D:
 func eat(mass_eaten):
 	mass += mass_eaten
 #	grow()
+
+
+func _on_border_area_entered(area: Area2D) -> void:
+	if area.is_in_group("algae"):
+		eat(area.mass)
+		area.queue_free()
