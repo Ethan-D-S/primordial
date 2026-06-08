b4225786586ecb0aac4489f2aa09777540c3c72a
Ethan Sahlstrom <sahlstromethan@gmail.com>
2026-06-08 12:20:27 -0700

    added regen animation

--- regen_energy_ability.gd
+++ regen_energy_ability.gd
@@ -6,13 +6,13 @@ extends Ability
 
 # player freezes while regenerating, making them vulnerable, hollow-knight style
 # time from button press to effect
-@export var regen_duration: float = 2.0
+@export var regen_duration: float = 2.5
 @export var energy_regen: int = 1
 
 var regen_timer: float = 0.0
 var is_active: bool = false
 
-
+@onready var animation: AnimatedSprite2D = $"RegenCircleAnimation"
 
 
 func _ready() -> void:
@@ -22,12 +22,16 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#reduce regen timer
 	if is_active:
+		animation.visible = true
+		animation.global_position = player.global_position + Vector2(0, 40)
 		regen_timer -= delta
 		player.velocity = Vector2.ZERO
 	
 		# cancel regen
 		if regen_timer <= 0:
 			is_active = false
+			animation.stop()
+			animation.visible = false
 			player.gain_energy(energy_regen)
 			return
 	
@@ -36,4 +40,7 @@ func activate():
 	if is_active:
 		return
 	is_active = true
+	print("activate called")
+	animation.play("recharge")
+	print("animation playing: ", animation.is_playing())
 	regen_timer = regen_duration
